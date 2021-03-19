5651783fb49141cead67df10364533dbbde9cb5a
saj96n <saj96n@gmail.com>
2021-03-19 12:14:53 +0100

    Add display of url slug after submit

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,14 +8,17 @@ import ky from "ky-universal";
 import Layout from "@components/Layout";
 import { Input } from "@components/Form/Input";
 import { Submit } from "@components/Form/Submit";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Home() {
   const [session, loading] = useSession();
   const { register, handleSubmit, errors, reset } = useForm<FormData>();
+  const [initial, setInitial] = useState(true);
 
   const [slug, setSlug] = useState<string | null>(null);
 
   const shortenLink = handleSubmit(async ({ url }: FormData) => {
+    setInitial(false);
     const short: Short = await ky.post("/api/links", { json: { url } }).json();
     setSlug(short.slug);
     reset();
@@ -25,24 +28,41 @@ export default function Home() {
       <main className="flex flex-1 flex-col pt-2 dark:bg-gray-800 dark:text-gray-200">
         <div className="flex-auto"></div>
         <div className="flex justify-center items-center flex-col">
-          <form
-            className="flex w-full flex-col max-w-screen-sm mb-4"
-            onSubmit={shortenLink}
-          >
-            <label htmlFor="url" className="text-2xl">
-              Url:
+          <AnimatePresence>
+            {!slug && <motion.form
+              action="/api/links"
+              method="POST"
+              initial={initial ? false : { opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className="flex w-full flex-col max-w-screen-sm mb-4"
+              onSubmit={shortenLink}
+            >
+              <label htmlFor="url" className="text-2xl">
+                Url:
             </label>
-            <div className="flex">
-              <Input
-                type="url"
-                name="url"
-                placeholder="https://example.com"
-                errors={errors}
-                ref={register({ required: "Please enter a valid url" })}
-              />
-              <Submit />
-            </div>
-          </form>
+              <div className="flex">
+                <Input
+                  type="url"
+                  name="url"
+                  placeholder="https://example.com"
+                  errors={errors}
+                  ref={register({ required: "Please enter a valid url" })}
+                />
+                <Submit />
+              </div>
+            </motion.form>}
+            {slug && <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="flex w-full flex-col max-w-screen-sm mb-4">
+              <label className="text-2xl">Shortened url:</label>
+              <div className="flex">
+                <Input
+                  type="url"
+                  name="shortened"
+                  value={slug}
+                />
+              </div>
+            </motion.div>}
+          </AnimatePresence>
         </div>
         <div className="flex-auto h-0 max-w-screen-lg mx-auto">
           <span>
