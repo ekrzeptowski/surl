776ab5ef4615228cb4df48d9d4ed261cffb67b61
saj96n <saj96n@gmail.com>
2021-03-25 20:40:06 +0100

    Add react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,21 @@
 import { Provider } from "next-auth/client";
 import { AppProps } from "next/dist/next-server/lib/router/router";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
 import "tailwindcss/tailwind.css";
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Provider session={pageProps.session}><Component {...pageProps} /></Provider>;
+  return (
+    <Provider session={pageProps.session}>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </Provider>
+  );
 }
 
 export default MyApp;
