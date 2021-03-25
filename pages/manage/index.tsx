6759138c53b5704a9d821d7afa9983694104be5e
saj96n <saj96n@gmail.com>
2021-03-26 00:43:30 +0100

    Add links management page

---
+++ pages/manage/index.tsx
@@ -0,0 +1,60 @@
+import { Main } from "@components/Main";
+import { Header, HeaderText } from "@components/Header";
+import Layout from "@components/Layout";
+import { useSession } from "next-auth/client";
+import { useQuery } from "react-query";
+import ky from "ky-universal";
+import { Short } from ".prisma/client";
+import { List, ListItem } from "@components/List";
+import Link from "next/link";
+
+import { BiCopy } from "@react-icons/all-files/bi/BiCopy";
+import CopyToClipboard from "react-copy-to-clipboard";
+import { createUrl } from "helpers/createUrl";
+const fetchShorts = (): Promise<Short[]> => ky.get("/api/links").json();
+
+export default function Manage() {
+  const [session, loading] = useSession();
+  const { isLoading, isError, data } = useQuery("shorts", fetchShorts);
+
+  if (!session && !loading) {
+    return (
+      <Layout>
+        <Header>
+          <HeaderText>Sign in to continue</HeaderText>
+        </Header>
+        <Main></Main>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout>
+      {!loading && (
+        <Header>
+          <HeaderText>Manage</HeaderText>
+        </Header>
+      )}
+      <Main>
+        {!loading && (
+          <List>
+            {data &&
+              data.map((cur) => (
+                <ListItem key={cur.slug}>
+                  <CopyToClipboard text={createUrl(cur.slug)}>
+                    <div className="flex py-4 cursor-pointer group items-center font-bold font-mono mr-4 flex-shrink-0">
+                      {cur.slug}
+                      <BiCopy className="transition-opacity opacity-0 group-hover:opacity-100" />
+                    </div>
+                  </CopyToClipboard>
+                  <Link href={`/manage/${cur.slug}`}>
+                    <a className="py-4">{cur.url}</a>
+                  </Link>
+                </ListItem>
+              ))}
+          </List>
+        )}
+      </Main>
+    </Layout>
+  );
+}
