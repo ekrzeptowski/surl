867a4430df21611bb972a9e760cb5b68fdc4b074
saj96n <saj96n@gmail.com>
2021-04-02 20:21:49 +0200

    Change page background

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,8 +1,14 @@
 import { FC, HTMLAttributes } from "react";
-export const Main: FC<HTMLAttributes<HTMLElement>> = ({ children, ...props }) => {
+export const Main: FC<HTMLAttributes<HTMLElement>> = ({
+  children,
+  ...props
+}) => {
   return (
-    <main className="flex flex-1 flex-col pt-2 dark:bg-gray-800 dark:text-gray-200" {...props}>
+    <main
+      className="flex flex-1 flex-col pt-2 bg-gray-100 dark:bg-gray-800 dark:text-gray-200"
+      {...props}
+    >
       {children}
     </main>
   );
-}
+};
