e672116004d638e9051920307d82706685cbf6a0
saj96n <saj96n@gmail.com>
2021-04-03 18:03:06 +0200

    Add more signin providers

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,9 +6,25 @@ import prisma from "@lib/prisma";
 
 export default NextAuth({
   providers: [
+    Providers.Discord({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    }),
+    Providers.Facebook({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    }),
+    Providers.GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     Providers.Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Providers.Twitter({
+      clientId: process.env.TWITTER_CLIENT_ID,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET,
     }),
   ],
   adapter: Adapters.Prisma.Adapter({ prisma }),
