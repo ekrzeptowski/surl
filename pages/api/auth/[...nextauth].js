0d34ba7b8c98156d34be0a840a18231ac9ab721e
ekrzeptowski <contact@ekrzeptowski.pl>
2022-12-14 23:31:38 +0100

    Disable facebook login provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
-import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 import TwitterProvider from "next-auth/providers/twitter";
@@ -12,30 +11,26 @@ export default NextAuth({
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET,
-    }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET
     }),
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }),
     TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID,
-      clientSecret: process.env.TWITTER_CLIENT_SECRET,
-    }),
+      clientSecret: process.env.TWITTER_CLIENT_SECRET
+    })
   ],
   adapter: PrismaAdapter(prisma),
   callbacks: {
     async session({ session, user }) {
       session.user.id = user.id;
       return session;
-    },
-  },
+    }
+  }
 });
